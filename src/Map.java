d652d710b0bda57ca8593226d1f7569d8b460be2
SleoNiiX <cardosomoreira.anthony@gmail.com>
2024-11-30 17:05:52 +0100

    Squelette d'archive pour les livrables

---
+++ src/Map.java
@@ -0,0 +1,5 @@
+class Map {
+    boolean[][] bombes; // 1 = bombe, 0 = vide
+    int[] indiceDepart; // première valeur sur l'axe des x puis y
+    int[] indiceFin;
+}
--- src/VivreOuSurvivre.java
+++ src/VivreOuSurvivre.java
@@ -1,5 +1,7 @@
 class VivreOuSurvivre extends Program {
+    final char cBombe = '*';
+
     void algorithm() {
         println("tacos");
-    }
+    } 
 }
