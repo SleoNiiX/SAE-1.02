5ae2487095e50db55c13e5573b237524174fc0f8
SleoNiiX <cardosomoreira.anthony@gmail.com>
2024-12-01 15:54:29 +0100

    Ajout avec test de boolean avancerNord (...) boolean estSurBombe (...) Et ajout type Joueur

---
+++ src/Joueur.java
@@ -0,0 +1,4 @@
+class Joueur {
+    int[] positionActuel; // Par défault le départ de la map
+    int vieRestante = 3;
+}
--- src/VivreOuSurvivre.java
+++ src/VivreOuSurvivre.java
@@ -4,19 +4,84 @@ class VivreOuSurvivre extends Program {
     final char cDepart = 'D';
     final char cFin = 'F';
 
-    void algorithm() {
+    void _algorithm() {
         // Création d'une map juste pour les tests 
         Map mapParDefaut = newMap(9, new int[]{0,0}, new int[]{8,8});
         mapParDefaut.bombes = new boolean[][]{{false,true,true,true,true,true,true,true,true},
-                                   {false,false,false,false,true,true,true,true,true},
-                                   {true,true,true,false,false,true,true,true,true},
-                                   {true,true,true,true,false,false,true,true,true},
-                                   {true,true,true,true,true,false,false,true,true},
-                                   {true,true,true,true,true,true,false,false,true},
-                                   {true,true,true,true,true,true,true,false,true},
-                                   {true,true,true,true,true,true,true,false,true},
-                                   {true,true,true,true,true,true,true,false,false}};
-        afficheMap(mapParDefaut);
+                                              {false,false,false,false,true,true,true,true,true},
+                                              {true,true,true,false,false,true,true,true,true},
+                                              {true,true,true,true,false,false,true,true,true},
+                                              {true,true,true,true,true,false,false,true,true},
+                                              {true,true,true,true,true,true,false,false,true},
+                                              {true,true,true,true,true,true,true,false,true},
+                                              {true,true,true,true,true,true,true,false,true},
+                                              {true,true,true,true,true,true,true,false,false}};
+
+        // Map pour le bloc avancer de n case
+        Map map1 = newMap(3, new int[]{2,1}, new int[]{0,1});
+        viderMap(map1);
+
+        afficheMap(map1);
+
+    }
+
+    /* ========================= */
+    /* Fonction de Base de blocs */
+    /* ========================= */
+
+    void testAvancerNord () {
+        Map map1 = newMap(3, new int[]{2,1}, new int[]{0,1});
+        viderMap(map1);
+        Joueur joueur1 = newJoueur(map1);
+
+        assertTrue(avancerNord(joueur1, map1, 2));
+        map1.bombes[1][1] = true;
+        joueur1.positionActuel = map1.indiceDepart;
+        assertFalse(avancerNord(joueur1, map1, 2));
+    }
+
+    boolean avancerNord (Joueur j, Map m, int nombreCases) {
+        int idxL;
+        boolean echec = false;
+        int cases = 1; 
+        
+        while (cases <= nombreCases && !echec) {
+            idxL = j.positionActuel[0]-1;
+            if (idxL < 0) {
+                return false;
+            }
+            j.positionActuel[0] = idxL;
+            echec = estSurBombe(j, m);
+            cases++;
+        }
+
+        return !echec;
+    }
+
+
+    void testEstSurBombe () {
+        Map map1 = newMap(3, new int[]{2,1}, new int[]{0,1});
+        viderMap(map1);
+        Joueur joueur1 = newJoueur(map1);
+
+        assertFalse(estSurBombe(joueur1, map1));
+        map1.bombes[1][1] = true;
+        joueur1.positionActuel = new int[]{1,1};
+        assertTrue(estSurBombe(joueur1, map1));
+    }
+
+    boolean estSurBombe (Joueur j, Map m) {
+        return m.bombes[j.positionActuel[0]][j.positionActuel[1]];
+    }
+
+    /* ============================== */
+    /* Fonction de Base sur le joueur */
+    /* ============================== */
+
+    Joueur newJoueur (Map m) {
+        Joueur j = new Joueur();
+        j.positionActuel = m.indiceDepart;
+        return j;
     }
 
     /* =========================== */
